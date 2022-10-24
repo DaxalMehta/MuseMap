945715fc8ef9d2257660ea46f86161de5f09d2e1
Daxal_Mehta <daxalmehta@gmail.com>
2022-10-24 17:02:01 +0200

    Updated

--- inputs.py
+++ inputs.py
@@ -1,19 +1,14 @@
 import numpy as np
 #####Data dir####
 fil="NGC1366.fits"
-bin_input="bin_inp.txt"
-bin_output="bin_out.txt"
-bins="bins.txt"
-fit_out="fitting_output"
-fit_pdf="fits.pdf"
-
-###Binning type###
-vor=False
-normal_binning_size=10 #length of one square bin
 
 ####Line####
-line=[6562,5007]
+line=[5007]
 z=0.003949
+
+###Binning type###
+binning="Normal" 		#"Adaptive" for another case
+normal_binning_size=10 		#length of one square bin
 
 ###plots###
 plot=True
